9eb7a2b9d05d30d8e1849c19c1e28e121d6d4c81
Wondimu Thomas <binimakifle@gmail.com>
2020-01-20 02:19:12 +0300

    Changes to be committed:

---
+++ doctor_homepage.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>DOCTOR HOME PAGE</title>
+</head>
+<body>
+<h1>welcome to doctor home page</h1>
+<p> this is doctor home page the user is only doctor in this page he can do many things like give appointment for patient order medicine and other relative things.</p>
+</body>
+</html>
